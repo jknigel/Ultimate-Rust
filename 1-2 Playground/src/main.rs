ce31acda8d791ac6b9712df10a633738cccfdede
Nigel Ang <ajknigel360@gmail.com>
2025-08-03 17:30:10 +0000

    Update 2.3

--- 1-2 Playground/src/main.rs
+++ 1-2 Playground/src/main.rs
@@ -1,3 +1,10 @@
+fn add_five(num:u32) -> u32 {
+	let result:u32 = num + 5;
+    return result
+}
+
 fn main() {
     println!("Hello, world!");
+    let my_num:u32 = add_five(7);
+    println!("My lucky number is {}", my_num);
 }
