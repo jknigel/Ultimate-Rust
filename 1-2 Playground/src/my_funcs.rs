5093ae99c5f216a60b298e50647e622baa489efd
Nigel Ang <ajknigel360@gmail.com>
2025-08-05 08:37:52 +0000

    Update 2.5

--- 1-2 Playground/src/my_funcs.rs
+++ 1-2 Playground/src/my_funcs.rs
@@ -7,3 +7,16 @@ pub fn multiply_five(num:u32) -> u32 {
 	let result:u32 = num * 5;
     return result
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_five_test() {
+        let x:u32 = 100;
+        let y:u32 = add_five(x);
+        println!("Test: {}", y);
+        assert_eq!(y, 105);
+    }
+}
--- 1-2 Playground/src/other_funcs/other_funcs1.rs
+++ 1-2 Playground/src/other_funcs/other_funcs1.rs
@@ -7,3 +7,16 @@ pub fn divide_five(num:u32) -> u32 {
 	let result:u32 = num / 5;
     return result
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn divide_five_test() {
+        let x:u32 = 100;
+        let y:u32 = divide_five(x);
+        println!("Test: {}", y);
+        assert_eq!(y, 21);
+    }
+}
