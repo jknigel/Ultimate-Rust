e4b833175edda4ae688c5ae35f583a1815fffc40
Nigel Ang <ajknigel360@gmail.com>
2025-08-19 10:51:52 +0000

    Update 6.1 - Result Enum with Generics

--- 4-1 Create/create/src/m1_enums.rs
+++ 4-1 Create/create/src/m1_enums.rs
@@ -7,9 +7,23 @@ enum CarColour {
     Rainbow
 }
 
+#[derive(Debug)]
+enum GivenResult<T,E> {
+    Ok(T),
+    Err(E)
+}
+
 fn create_car_colour_rainbow() -> CarColour {
     let my_car_colour:CarColour = CarColour::Rainbow;
     return my_car_colour
+}
+
+fn check_under_five(num_check:u8) -> GivenResult<u8, String> {
+    if num_check < 5 {
+        return GivenResult::Ok(num_check)
+    } else {
+        return GivenResult::Err("Not under 5!".to_string())
+    }
 }
 
 #[cfg(test)]
@@ -20,5 +34,11 @@ mod test {
     fn tests_enums() {
         let car_colour:CarColour = create_car_colour_rainbow();
         dbg!(car_colour);
+
+        let is_under_five_result:GivenResult<u8, String> = check_under_five(2);
+        dbg!(is_under_five_result);
+
+        let is_not_under_five_result:GivenResult<u8, String> = check_under_five(7);
+        dbg!(is_not_under_five_result);
     }
 }
