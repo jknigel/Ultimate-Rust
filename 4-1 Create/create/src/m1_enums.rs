ec123d334f6ae673f90e42b34f67a1a112c1b464
Nigel Ang <ajknigel360@gmail.com>
2025-08-21 06:58:56 +0000

    Update 6.2

--- 4-1 Create/create/src/m1_enums.rs
+++ 4-1 Create/create/src/m1_enums.rs
@@ -13,6 +13,12 @@ enum GivenResult<T,E> {
     Err(E)
 }
 
+#[derive(Debug)]
+enum GivenOption<T> {
+    None,
+    Some(T)
+}
+
 fn create_car_colour_rainbow() -> CarColour {
     let my_car_colour:CarColour = CarColour::Rainbow;
     return my_car_colour
@@ -23,6 +29,33 @@ fn check_under_five(num_check:u8) -> GivenResult<u8, String> {
         return GivenResult::Ok(num_check)
     } else {
         return GivenResult::Err("Not under 5!".to_string())
+    }
+}
+
+//This function uses built-in result, no need define enum
+fn check_under_five_built_in(num_check:u8) -> Result<u8, String> {
+    if num_check < 5 {
+        return Result::Ok(num_check)
+    } else {
+        return Result::Err("Not under 5!".to_string())
+    }
+}
+
+fn remainder_zero(num_check:f32) -> GivenOption<f32> {
+    let remainder:f32 = num_check % 10.0;
+    if remainder != 0.0 {
+        return GivenOption::Some(remainder)
+    } else {
+        return GivenOption::None
+    }
+}
+
+fn remainder_zero_built_in(num_check:f32) -> Option<f32> {
+    let remainder:f32 = num_check % 10.0;
+    if remainder != 0.0 {
+        return Some(remainder)
+    } else {
+        return None
     }
 }
 
@@ -40,5 +73,17 @@ mod test {
 
         let is_not_under_five_result:GivenResult<u8, String> = check_under_five(7);
         dbg!(is_not_under_five_result);
+
+        let build_in_under_five_result:Result<u8, String> = check_under_five_built_in(3);
+        dbg!(build_in_under_five_result);
+
+        let remainder:GivenOption<f32> = remainder_zero(12.2);
+        dbg!(remainder);
+
+        let no_remainder:GivenOption<f32> = remainder_zero(10.0);
+        dbg!(no_remainder);
+
+        let remainder_built_in:Option<f32> = remainder_zero_built_in(18.8);
+        dbg!(remainder_built_in);
     }
 }
