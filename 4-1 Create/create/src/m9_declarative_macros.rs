28cfb3190d00a6dadd58c31244210911e99142b6
Nigel Ang <ajknigel360@gmail.com>
2025-09-04 05:36:55 +0000

    Update 6.14 Declarative Macros with Reptitions

--- 4-1 Create/create/src/m9_declarative_macros.rs
+++ 4-1 Create/create/src/m9_declarative_macros.rs
@@ -15,16 +15,36 @@ mod tests {
         }
     }
 
+    //Initialize vector declarative macro
+    macro_rules! my_vec {
+        ($($x: expr),+ ) => {
+            {
+                let mut temp_vec = Vec::new();
+
+                $(
+                    temp_vec.push($x);
+                )+
+                temp_vec
+            }
+        }
+    }
+
     #[test]
     fn tests_declarative_macro() {
         println!("Hello 1");
         dbg!("Hello 2");
+
         let x:Vec<i32> = vec!(1,2,3);
         let formatted:String = format!("Hello 3 with vec {:?}", x);
         dbg!(formatted);
         println!("------");
+
         let some_var:String = mad_skills!(i32);
         dbg!(some_var);
+
+        let mut y:Vec<i32> = my_vec!(1);
+
+        dbg!(y);
     
     }
 }
