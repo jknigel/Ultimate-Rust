c1d1dd9ed0599ee79ed09942404f30b9f5906136
Nigel Ang <ajknigel360@gmail.com>
2025-09-07 16:50:10 +0000

    Update 7.3 Smart Pointers with Rc

--- 5-2 Smart Pointers/smart_pointers/src/m11_smart_pointers.rs
+++ 5-2 Smart Pointers/smart_pointers/src/m11_smart_pointers.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 
 mod test {
+    use std::rc::{Rc, Weak};
+    use std::cell::RefCell;
+
     #[test]
     #[allow(dead_code, unused_variables)]
     fn tests_box_smart_pointers() {
@@ -20,6 +23,27 @@ mod test {
             )}
         );
         dbg!(nodes);
+
+    }
+
+    #[test]
+    #[allow(dead_code, unused_variables)]
+    fn tests_reference_counter() {
+        
+        let mut x:Rc<i32> = Rc::new(50); //Rc is reference counter: it will count number of references to it
+        let y:Rc<i32> = Rc::clone(&x);
+
+        x = Rc::new(70);
+
+        dbg!(y);
+        dbg!(x);
+
+        let a:Rc<RefCell<i32>> = Rc::new(RefCell::new(80)); //This allows us to mutate all the values that share reference with variable a
+        let b:Rc<RefCell<i32>> = Rc::clone(&a);
+
+        *a.borrow_mut() = 100; //can use *b.borrow_mut() as well
+        dbg!(a);
+        dbg!(b);
 
     }
 }
