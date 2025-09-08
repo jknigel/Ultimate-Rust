674ada026a27a402f70edd8b3733749285e975d8
Nigel Ang <ajknigel360@gmail.com>
2025-09-08 07:31:43 +0000

    Update 7.4 Smart Pointers with Weak

--- 5-2 Smart Pointers/smart_pointers/src/m11_smart_pointers.rs
+++ 5-2 Smart Pointers/smart_pointers/src/m11_smart_pointers.rs
@@ -48,6 +48,48 @@ mod test {
         *b.borrow_mut() = 100; //can use *b.borrow_mut() as well
         dbg!(&a);
         dbg!(&b);
-
     }
+
+    #[test]
+    #[allow(dead_code, unused_variables)]
+    fn tests_smart_pointers_weak() {
+        #[derive(Debug)]
+        struct House {
+            address_number:u16,
+            street:String,
+            furniture:RefCell<Vec<Rc<Furniture>>>
+        }
+
+        #[derive(Debug)]
+        struct Furniture {
+            id:String,
+            description:String,
+            house:Weak<House> //A weak pointer can reference an object without owning it
+        }
+
+        let house1:Rc<House> = Rc::new(House {
+            address_number: 123,
+            street: "coding avenue".to_string(),
+            furniture: RefCell::new(vec!())
+        });
+
+        let table:Rc<Furniture> = Rc::new(Furniture {
+            id: "Table1".to_string(),
+            description: "Kitchen Table".to_string(),
+            house: Rc::downgrade(&house1) //Ownership has shifted here, so house1 cannot be used elsewhere
+        });
+
+        let desk:Rc<Furniture> = Rc::new(Furniture {
+            id: "Desk1".to_string(),
+            description: "Office Desk".to_string(),
+            house: Rc::downgrade(&house1)
+        });
+
+        house1.furniture.borrow_mut().push(Rc::clone(&table));
+        house1.furniture.borrow_mut().push(Rc::clone(&desk));
+
+        dbg!(house1);
+    }
+
+    
 }
